53f0fc3352ddc9b638ef53b7e2efecea30351cdb
Hidayattt24 <hidayat.22@mhs.usk.ac.id>
2024-12-17 20:37:14 +0700

    navbar mobile dan desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,21 +25,34 @@ const Navbar = () => {
 
         {/* Mobile link list */}
         <div
-          className={`w-full h-screen flex flex-col items-center justify-center absolute top-16  transition-all duration-300 ${
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16  transition-all duration-300 ${
             open ? "right-0" : "-right-[100%]"
           }`}
         >
-          <ul className="text-white">
-            <li className="py-2">Home</li>
-            <li className="py-2">About</li>
-            <li className="py-2">Services</li>
-            <li className="py-2">Contact</li>
-          </ul>
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </a>
         </div>
       </div>
 
       {/* DESKTOP MENU */}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login 👋
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
