01d5807f9ce48877a6b7e704cbab0bc93c790e87
Hidayattt24 <hidayat.22@mhs.usk.ac.id>
2024-12-20 15:12:09 +0700

    Image/reducingImageSize-2

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -7,7 +7,11 @@ const FeaturedPosts = () => {
       {/* First */}
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* Image */}
-        <Image src="featured1.jpeg" className="rounded-3xl object-cover" />
+        <Image
+          src="featured1.jpeg"
+          className="rounded-3xl object-cover w-full h-full"
+          w="895"
+        />
 
         {/* Detail */}
         <div className="flex items-center gap-4">
@@ -26,13 +30,16 @@ const FeaturedPosts = () => {
       </div>
 
       {/* Others */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-4">
+      <div className="w-full lg:h-1/2 flex flex-col gap-4">
         {/* second */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured2.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured2.jpeg"
+              className="rounded-3xl object-cove w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and title */}
           <div className="w-2/3">
             {/* details */}
@@ -53,10 +60,13 @@ const FeaturedPosts = () => {
 
         {/* third */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured3.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured3.jpeg"
+              className="rounded-3xl object-cove w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and title */}
           <div className="w-2/3">
             {/* details */}
@@ -77,10 +87,13 @@ const FeaturedPosts = () => {
 
         {/* fourth */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured4.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured4.jpeg"
+              className="rounded-3xl object-cove w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and title */}
           <div className="w-2/3">
             {/* details */}
