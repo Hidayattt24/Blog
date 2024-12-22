49b8b0dc30c55857e04fea5ec82b94a077733dfb
Hidayattt24 <hidayat.22@mhs.usk.ac.id>
2024-12-22 23:33:19 +0700

    sideMenuDesign

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,15 +3,15 @@ import PostListItem from "./PostListItem";
 const PostList = () => {
   return (
     <div className="flex flex-col gap-12 mb-8">
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
     </div>
   );
 };
